6253c4b38ceb4862944b2d8bb927f93c460f3b0a
ophiase <ophiase@outlook.com>
2025-02-09 14:52:09 +0100

    feature: shuffle partition

--- logic/graph_generation.py
+++ logic/graph_generation.py
@@ -40,16 +40,20 @@ class GraphGeneration:
 
         return g
 
-    #########################################
+    ###################################################################################
 
     @staticmethod
-    def _partition_nodes(n_nodes: int, n_partitions: int = 4) -> List[List[int]]:
+    def _partition_nodes(
+        n_nodes: int,
+        n_partitions: int = 4, shuffle: bool = True
+    ) -> List[List[int]]:
         """
         Partitions `n_nodes` nodes into `n_partitions` groups as evenly as possible.
 
         Args:
             n_nodes (int): Total number of nodes to partition.
             n_partitions (int): Number of partitions to create. Default is 4.
+            shuffle (bool): Whether to shuffle the nodes before partitioning. Default is True.
 
         Returns:
             List[List[int]]: A list of lists, where each sublist represents a partition of nodes.
@@ -58,27 +62,37 @@ class GraphGeneration:
             >>> GraphGeneration._partition_nodes(10, 3)
             [[0, 1, 2, 3], [4, 5, 6], [7, 8, 9]]
         """
+        nodes = list(range(n_nodes))
+        if shuffle:
+            random.shuffle(nodes)
+
         groups = []
         base = n_nodes // n_partitions
         remainder = n_nodes % n_partitions
         start = 0
         for i in range(n_partitions):
             group_size = base + (1 if i < remainder else 0)
-            groups.append(list(range(start, start + group_size)))
+            groups.append(nodes[start:start + group_size])
             start += group_size
 
         return groups
 
     @staticmethod
-    def tp3_graph(n: int, p: float, q: float, n_partitions: int = 4) -> nx.Graph:
+    def tp3_graph(
+        n_nodes: int, p: float, q: float,
+        n_partitions: int = 4, shuffle: bool = True
+    ) -> nx.Graph:
         g = nx.Graph()
-        g.add_nodes_from(range(n))
-        groups = GraphGeneration._partition_nodes(n, n_partitions=n_partitions)
+        g.add_nodes_from(range(n_nodes))
+        groups = GraphGeneration._partition_nodes(
+            n_nodes, n_partitions=n_partitions, shuffle=shuffle)
+
         for group in groups:
             for i in range(len(group)):
                 for j in range(i + 1, len(group)):
                     if random.random() < p:
                         g.add_edge(group[i], group[j])
+
         for i in range(len(groups)):
             for j in range(i + 1, len(groups)):
                 for u in groups[i]:
@@ -101,8 +115,8 @@ def main() -> None:
     graph_params = [
         ("Erdos-Renyi p", GraphGeneration.erdos_graph_p, (10, 0.3)),
         ("Erdos-Renyi m", GraphGeneration.erdos_graph_m, (10, 15)),
-        ("TP3 Graph", GraphGeneration.tp3_graph, (16, 0.8, 0.2)),
-        ("TP3 Graph", GraphGeneration.tp3_graph, (50, 0.95, 0.02))
+        ("TP3 Graph", GraphGeneration.tp3_graph, (16, 0.8, 0.2, 4, True)),
+        ("TP3 Graph", GraphGeneration.tp3_graph, (50, 0.95, 0.02, 4, False))
     ]
 
     plt.figure(figsize=(15, 5))
