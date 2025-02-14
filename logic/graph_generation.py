6cba347c2d05b000372c4393d17fb4948749321a
ophiase <ophiase@outlook.com>
2025-02-14 17:44:13 +0100

    minimal

--- logic/graph_generation.py
+++ logic/graph_generation.py
@@ -79,32 +79,28 @@ class GraphGeneration:
 
         return g
 
-    # def generate_erdos_m_partition_model(
-    #     partition: List[List[int]], 
-    #     m_edges: float: q: float
-    # ) -> nx.Graph:
-    #     n_nodes = sum([len(group) for group in partition])
+    def generate_erdos_m_partition_model(
+        partition: List[List[int]],
+        m_edges: float, q: float
+    ) -> nx.Graph:
+        n_nodes = sum([len(group) for group in partition])
 
-    #     g = nx.Graph()
-    #     g.add_nodes_from(range(n_nodes))
+        g = nx.Graph()
+        g.add_nodes_from(range(n_nodes))
 
-    #     # m edges
-    #     for group in partition:
-    #         edges_added = 0
-    #         while edges_added < m_edges:
-    #             u = random.randint(0, n_vertices - 1)
-    #             v = random.randint(0, n_vertices - 1)
-    #             if u != v and not g.has_edge(u, v):
-    #                 g.add_edge(u, v)
-    #                 edges_added += 1
+        # m edges per group
+        for group in partition:
+            possible_edges = [(u, v) for u in group for v in group if u < v]
+            selected_edges = random.sample(possible_edges, min(m_edges, len(possible_edges)))
+            g.add_edges_from(selected_edges)
 
-    #     # edges with a probability of q
-    #     for group1, group2 in _external_pair(len(partition)):
-    #         for e1, e2 in _cartesian_product(partition[group1], partition[group2]):
-    #             if random.random() < q:
-    #                 g.add_edge(e1, e2)
+        # edges with a probability of q
+        for group1, group2 in _external_pair(len(partition)):
+            for e1, e2 in _cartesian_product(partition[group1], partition[group2]):
+                if random.random() < q:
+                    g.add_edge(e1, e2)
 
-    #     return g
+        return g
 
 ###################################################################################
 
@@ -112,15 +108,26 @@ class GraphGeneration:
 def main() -> None:
     partition_shuffled = NodePartition.partition_list(50, as_set=False)
     partition_unshuffled = NodePartition.partition_list(
-        50, shuffle=False, as_set=False)
+        30, shuffle=False, as_set=False)
 
     graph_params = [
         ("Erdos-Renyi p", GraphGeneration.erdos_graph_p(10, 0.3), None),
         ("Erdos-Renyi m", GraphGeneration.erdos_graph_m(10, 15), None),
-        ("TP3 Graph", GraphGeneration.generate_erdos_p_partition_model(
+
+        ("Partitioned Erdos-Renyi p", GraphGeneration.generate_erdos_p_partition_model(
             partition_shuffled, p=0.8, q=0.2), partition_shuffled),
-        ("TP3 Graph", GraphGeneration.generate_erdos_p_partition_model(
-            partition_unshuffled, p=0.95, q=0.02), partition_unshuffled)
+        ("Partitioned Erdos-Renyi p", GraphGeneration.generate_erdos_p_partition_model(
+            partition_unshuffled, p=0.95, q=0.02), partition_unshuffled),
+        # (
+        #     "Partitioned Erdos-Renyi m", 
+        #     GraphGeneration.generate_erdos_m_partition_model(partition_shuffled, m_edges=10, q=0.2), 
+        #     partition_shuffled
+        # ),
+        # (
+        #     "Partitioned Erdos-Renyi m", 
+        #     GraphGeneration.generate_erdos_m_partition_model(partition_unshuffled, m_edges=10, q=0.02), 
+        #     partition_unshuffled
+        # )
     ]
 
     plt.figure(figsize=(15, 5))
